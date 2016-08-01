bce31f2415a5f5f8d592a25263d107cfb589d3fd
Matthew Kory <matthewskory@gmail.com>
2016-08-01 10:30:47 -0400

    deleted old broken log

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-	// User model created using sequelize
-	var User = sequelize.define('user', {
-		username: { type: DataTypes.STRING, unique: true },
-		passwordhash: DataTypes.STRING
-	});	
-
-	return User;
-};
